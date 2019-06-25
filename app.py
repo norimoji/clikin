0660af1a710b6f124b1fc92d5feadb1b4662438f
norimoji <zimmerzing@gmail.com>
2019-06-25 16:25:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,93 @@
- From flask import Flask, request, abort 
-  From linebot import ( 
-  LineBotApi, WebhookHandler 
-  ) 
-  From linebot.exceptions import ( 
-  InvalidSignatureError 
-  ) 
-  From linebot.models import * 
-  App = Flask( __name__ ) 
-  # Channel Access Token 
-  Line_bot_api = LineBotApi( 'JAkiT/xizWqtsFMxzFZHUVxx5yacRkBjSDgvU+ulNTD6L/HfeVwtelULk5nPmQxLro3UgB5TjhPl5LIfXOYyXuxbJcRsTQWBcXKjUw9YXZZVlwbCWTBXmnsLaBqNSTT8f5OIk/aV6kffghU4M3sNPAdB04t89/1O/w1cDnyilFU=' ) 
-  # Channel Secret 
-  Handler = WebhookHandler( '3ec036fffe82cfcc225679876cb63c78' ) 
-  #Listen all Post requests from /callback 
-  @app.route ( " /callback " , methods = [ ' POST ' ]) 
-  Def callback (): 
-  # get X-Line-Signature header value 
-  Signature = request.headers[ ' X-Line-Signature ' ] 
-  # get request body as text 
-  Body = request.get_data( as_text = True ) 
-  App.logger.info( " Request body: " + body) 
-  # handle webhook body 
-  Try : 
-  Handler.handle(body, signature) 
-  Except InvalidSignatureError: 
-  Abort( 400 ) 
-  Return ' OK ' 
-  #处理消息 
-  @handler.add (MessageEvent, message = TextMessage) 
-  Def handle_message ( event ): 
-  Message = TextSendMessage( text = event.message.text) 
-  Line_bot_api.reply_message(event.reply_token, message) 
-  Import os 
-  If __name__ == " __main__ " : 
-  Port = int (os.environ.get( ' PORT ' , 5000 )) 
-  App.run( host = ' 0.0.0.0 ' , port = port) 
+import os
+import sys
+import wsgiref.simple_server
+from argparse import ArgumentParser
+
+from builtins import bytes
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage
+)
+from linebot.utils import PY3
+
+LineChannelSecret = '3ec036fffe82cfcc225679876cb63c78'
+LineChannelAccessToken = 'JAkiT/xizWqtsFMxzFZHUVxx5yacRkBjSDgvU+ulNTD6L/HfeVwtelULk5nPmQxLro3UgB5TjhPl5LIfXOYyXuxbJcRsTQWBcXKjUw9YXZZVlwbCWTBXmnsLaBqNSTT8f5OIk/aV6kffghU4M3sNPAdB04t89/1O/w1cDnyilFU='
+
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv(LineChannelSecret, None)
+channel_access_token = os.getenv(LineChannelSecret, None)
+if channel_secret is None:
+    print(LineChannelSecret)
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(LineChannelAccessToken)
+parser = WebhookParser(LineChannelSecret)
+
+
+def application(environ, start_response):
+    # check request path
+    if environ['PATH_INFO'] != '/callback':
+        start_response('404 Not Found', [])
+        return create_body('Not Found')
+
+    # check request method
+    if environ['REQUEST_METHOD'] != 'POST':
+        start_response('405 Method Not Allowed', [])
+        return create_body('Method Not Allowed')
+
+    # get X-Line-Signature header value
+    signature = environ['HTTP_X_LINE_SIGNATURE']
+
+    # get request body as text
+    wsgi_input = environ['wsgi.input']
+    content_length = int(environ['CONTENT_LENGTH'])
+    body = wsgi_input.read(content_length).decode('utf-8')
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        start_response('400 Bad Request', [])
+        return create_body('Bad Request')
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Hi there human')
+        )
+
+    start_response('200 OK', [])
+    return create_body('OK')
+
+
+def create_body(text):
+    if PY3:
+        return [bytes(text, 'utf-8')]
+    else:
+        return text
+
+
+if __name__ == '__main__':
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    options = arg_parser.parse_args()
+
+    httpd = wsgiref.simple_server.make_server('', options.port, application)
+    httpd.serve_forever()
